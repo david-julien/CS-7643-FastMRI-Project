069600a51e5269456f455d9e56231b6a04ed7d75
david Julien <davidjulien@davids-MBP.cgocable.net>
2024-04-13 19:38:48 -0400

    another attempt to try and fix the tqdm printing to new lines issue

--- fastmri_examples/unet/train_unet_demo.py
+++ fastmri_examples/unet/train_unet_demo.py
@@ -29,33 +29,21 @@ class MyProgressBar(pl.callbacks.TQDMProgressBar):
         bar = super().init_train_tqdm()
         bar.position = 0
         bar.leave = True
-        # if not sys.stdout.isatty():
-        #     bar.disable = True
         return bar
-
 
     def init_validation_tqdm(self):
         bar = super().init_validation_tqdm()
-        bar.position = 0
-        bar.leave = True
-        # if not sys.stdout.isatty():
-        #     bar.disable = True
+        bar.disable = True
         return bar
 
     def init_predict_tqdm(self):
         bar = super().init_predict_tqdm()
-        bar.position = 0
-        bar.leave = True
-        # if not sys.stdout.isatty():
-        #     bar.disable = True
+        bar.disable = True
         return bar
 
     def init_test_tqdm(self):
         bar = super().init_test_tqdm()
-        bar.position = 0
-        bar.leave = True
-        # if not sys.stdout.isatty():
-        #     bar.disable = True
+        bar.disable = True
         return bar
 
 
